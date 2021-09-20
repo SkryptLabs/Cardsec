56ae003407a38616f67d7df5ee70ade0a3f83d3d
root <root@LAPTOP-GKJ0PNL3.localdomain>
2021-09-20 23:28:42 +0530

    Added exhaustive Port scanning

--- cardsec/main.py
+++ cardsec/main.py
@@ -2,11 +2,14 @@ import typer
 import psutil
 import subprocess
 import distro
+from typing import Optional
+from cardsec.port_scanner import scan_ports
 
 GOOD=50
 OK=75
 
 def num_colour(start:str,num:int):
+    start=typer.style(start,fg=typer.colors.BRIGHT_WHITE)
     if num<=GOOD:
         end=typer.style(str(num)+'%', fg=typer.colors.GREEN, bold=True)
     elif num<=OK:
@@ -21,15 +24,16 @@ app = typer.Typer()
 @app.callback()
 def callback():
     """
-    Cardsec for SPOs
+    System and Security Assesment Tool for SPOs
     """
 
-@app.command()
+@app.command(short_help="Info about the system")
 def system():
     #cpu_load=(psutil.getloadavg()[2]/psutil.cpu_count())*100
-    cpu_load=psutil.cpu_percent(2)
+    cpu_load=psutil.cpu_percent()
     ram_load=psutil.virtual_memory()[2]
     disk_load=psutil.disk_usage('/')
+    
     try:
         node=subprocess.check_output(['cardano-node','version']).decode().split()[1]
     except: node="Not found."
@@ -37,12 +41,26 @@ def system():
         wallet=subprocess.check_output(['cardano-wallet','version']).decode().split()[0]
     except: wallet="Not found."
     
-    typer.secho("\n-------System Info---------",fg=typer.colors.MAGENTA)
+    typer.secho("\n-------System Info---------",fg=typer.colors.MAGENTA,bold=True)
     
     typer.echo(num_colour("CPU usage: ",cpu_load))
     typer.echo(num_colour("RAM usage: ",ram_load))
     typer.echo(num_colour("Disk usage: ",disk_load[3])+'\n')
+    
     typer.echo("Distro: "+distro.id()+' '+distro.version())
     typer.echo("Cardano-Node: "+node)
-    typer.echo("Cardano-Wallet: "+wallet)
-    typer.echo('\n')
+    typer.echo("Cardano-Wallet: "+wallet+'\n')
+    #typer.echo('\n')
+
+@app.command()
+def scan(ports: Optional[str] = typer.Argument(None)):
+    if ports is None:
+        typer.secho("Scan ports by: cardsec scan ports")
+    else:
+        port_list=scan_ports()
+        if port_list:
+            typer.secho("The following ports are open:",fg=typer.colors.BRIGHT_MAGENTA)
+            for i in port_list:
+                typer.echo(i)
+            typer.secho("Note: Only keep 2 outgoing ports open. (SSH & Node port) \n",fg=typer.colors.WHITE)
+        else: typer.secho("No ports open!",fg=typer.colors.RED)
---
+++ cardsec/port_scanner.py
@@ -0,0 +1,47 @@
+import socket
+import time
+import threading
+
+from queue import Queue
+socket.setdefaulttimeout(0.25)
+print_lock = threading.Lock()
+
+target = socket.gethostname()
+t_IP = socket.gethostbyname(target)
+print ('Starting scan on host: ', t_IP)
+
+port_list=set()
+q = Queue()
+
+def portscan(port):
+   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   try:
+      con = s.connect((t_IP, port))
+      with print_lock:
+          port_list.add(port)
+      con.close()
+   except:
+      pass
+
+def threader():
+   while True:
+      worker = q.get()
+      port_list.add(portscan(worker))
+      q.task_done()
+
+def scan_ports():
+   for x in range(100):
+            t = threading.Thread(target = threader)
+            t.daemon = True
+            t.start()
+
+   for worker in range(1, 500):
+            q.put(worker)
+
+   q.join()
+   port_list.discard(None)
+   if port_list:
+      return port_list
+   else: return False
+
+#print(scan_ports())
