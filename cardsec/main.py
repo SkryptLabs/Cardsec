44019ea3196f6cd4f286c070617a6581a59f62ba
root <root@LAPTOP-GKJ0PNL3.localdomain>
2021-09-20 14:42:34 +0530

    Added distro info and colours

--- cardsec/main.py
+++ cardsec/main.py
@@ -1,6 +1,19 @@
 import typer
 import psutil
+import subprocess
+import distro
 
+GOOD=50
+OK=75
+
+def num_colour(start:str,num:int):
+    if num<=GOOD:
+        end=typer.style(str(num)+'%', fg=typer.colors.GREEN, bold=True)
+    elif num<=OK:
+        end=typer.style(str(num)+'%', fg=typer.colors.YELLOW, bold=True)
+    else: typer.style(str(num)+'%', fg=typer.colors.RED, bold=True)
+
+    return start+end
 
 app = typer.Typer()
 
@@ -17,6 +30,19 @@ def system():
     cpu_load=psutil.cpu_percent(2)
     ram_load=psutil.virtual_memory()[2]
     disk_load=psutil.disk_usage('/')
-    typer.echo("CPU usage: "+ str(cpu_load)+'%')
-    typer.echo("RAM usage: "+ str(ram_load)+'%')
-    typer.echo("Disk usage: "+ str(disk_load[3])+'%')
+    try:
+        node=subprocess.check_output(['cardano-node','version']).decode().split()[1]
+    except: node="Not found."
+    try:
+        wallet=subprocess.check_output(['cardano-wallet','version']).decode().split()[0]
+    except: wallet="Not found."
+    
+    typer.secho("\n-------System Info---------",fg=typer.colors.MAGENTA)
+    
+    typer.echo(num_colour("CPU usage: ",cpu_load))
+    typer.echo(num_colour("RAM usage: ",ram_load))
+    typer.echo(num_colour("Disk usage: ",disk_load[3])+'\n')
+    typer.echo("Distro: "+distro.id()+' '+distro.version())
+    typer.echo("Cardano-Node: "+node)
+    typer.echo("Cardano-Wallet: "+wallet)
+    typer.echo('\n')
