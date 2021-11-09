a1bf2e7610f6b8f79fdec3d9fe5e635ae316392e
Adnan Khan <80961004+SkepX@users.noreply.github.com>
2021-11-09 17:53:33 +0530

    Update port_scanner.py

--- cardsec/port_scanner.py
+++ cardsec/port_scanner.py
@@ -1,47 +1,126 @@
-import socket
+import os
+import signal
 import time
+import socket
 import threading
-
+import sys
+import subprocess
 from queue import Queue
-socket.setdefaulttimeout(0.25)
-print_lock = threading.Lock()
+from datetime import datetime
 
-target = socket.gethostname()
-t_IP = socket.gethostbyname(target)
-#print ('Starting scan on host: ', t_IP)
+# Start Threader3000 with clear terminal
+subprocess.call('clear', shell=True)
 
-port_list=set()
-q = Queue()
+# Main Function
+def scan_ports():
+    socket.setdefaulttimeout(0.30)
+    print_lock = threading.Lock()
+    discovered_ports = []
 
-def portscan(port):
-   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   try:
-      con = s.connect((t_IP, port))
-      with print_lock:
-          port_list.add(port)
-      con.close()
-   except:
-      pass
 
-def threader():
-   while True:
-      worker = q.get()
-      port_list.add(portscan(worker))
-      q.task_done()
+    time.sleep(1)
+    target = socket.gethostname()
+    t_ip = socket.gethostbyname(target)
+    print ('Starting scan on host: ', t_ip)
+    print("-" * 60)
+    print("Time started: "+ str(datetime.now()))
+    print("-" * 60)
+    t1 = datetime.now()
 
-def scan_ports(port_range):
-   for x in range(100):
-            t = threading.Thread(target = threader)
-            t.daemon = True
-            t.start()
+    def portscan(port):
 
-   for worker in range(1, port_range):
-            q.put(worker)
+       s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+       
+       try:
+          conx = s.connect((t_ip, port))
+          with print_lock:
+             print("Port {} is open".format(port))
+             discovered_ports.append(str(port))
+          conx.close()
 
-   q.join()
-   port_list.discard(None)
-   if port_list:
-      return port_list
-   else: return False
+          
+          
 
-#print(scan_ports())
+       except (ConnectionRefusedError, AttributeError, OSError):
+          pass
+
+    def threader():
+       while True:
+          worker = q.get()
+          portscan(worker)
+          q.task_done()
+      
+    q = Queue()
+     
+    #startTime = time.time()
+     
+    for x in range(200):
+       t = threading.Thread(target = threader)
+       t.daemon = True
+       t.start()
+
+    for worker in range(1, 65536):
+       q.put(worker)
+
+    q.join()
+
+    t2 = datetime.now()
+    total = t2 - t1
+    if discovered_ports:
+      print("Port scan completed in "+str(total))
+      print("\nHappy Secure Minting!")
+    else:
+      print("No ports were open!")
+      print("\nHappy Secure Minting!")
+
+#VULNERABILITY DETECTION  
+   #  print("Scan these ports for vulnerability detection:")
+   #  print("*" * 60)
+   #  print("nmap -p{ports} -sV -sC -T4 -Pn -oA {ip} {ip}".format(ports=",".join(discovered_ports), ip=target))
+   #  print("*" * 60)
+   #  outfile = "nmap -p{ports} -sV -sC -Pn -T4 -oA {ip} {ip}".format(ports=",".join(discovered_ports), ip=target)
+   #  t3 = datetime.now()
+   #  total1 = t3 - t1
+
+#Nmap
+
+   #  def automate():
+   #     choice = '0'
+   #     while choice =='0':
+   #        print("Would you like to run a vulnerability scan or quit?")
+   #        print("-" * 60)
+   #        print("1 = Run suggested Nmap scan")
+   #        print("2 = Exit to terminal")
+   #        print("-" * 60)
+   #        choice = input("Option Selection: ")
+         #  if choice == "1":
+         #     try:
+         #        print(outfile)
+         #        os.mkdir(target)
+         #        os.chdir(target)
+         #        os.system(outfile)
+         #        convert = "xsltproc "+target+".xml -o "+target+".html"
+         #        os.system(convert)
+         #        t3 = datetime.now()
+         #        total1 = t3 - t1
+         #        print("-" * 60)
+         #        print("Combined scan completed in "+str(total1))
+         #        print("Press enter to quit...")
+         #        input()
+         #     except FileExistsError as e:
+         #        print(e)
+         #        exit()
+   #        if choice =="2":
+   #           print("\nHappy Secure Minting!")
+   #           sys.exit()
+   #        else:
+   #           print("Please make a valid selection")
+   #           automate()
+   #  automate()
+
+if __name__ == '__main__':
+    try:
+        scan_ports()
+    except KeyboardInterrupt:
+        print("\nHappy Secure Minting!")
+        quit()
